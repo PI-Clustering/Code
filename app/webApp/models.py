6e60d978c601cbc298806d80993ec2244561a829
Fatemeh Ghasemi <fa_maess@Fatemehs-MacBook-Pro.local>
2022-02-07 14:03:43 +0100

    the upload form

--- app/webApp/models.py
+++ app/webApp/models.py
@@ -21,3 +21,10 @@ class Benchmark(models.Model):
 
     def __str__(self):
         return self.algo_type + self.size
+
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
