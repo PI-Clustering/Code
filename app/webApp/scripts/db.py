0a1f85141c2a3772e9d7c1e218e6f110ec6c3226
thomaspickles <thomas.pickles@gmail.com>
2022-03-10 09:14:08 +0100

    Oops. Added missing file from previous bad commit

---
+++ app/webApp/scripts/db.py
@@ -0,0 +1,46 @@
+from django.db import connection
+from collections import defaultdict
+
+
+def get_data():
+
+    query_string = '''select b.algo_type, b.data_set, b.size, d.iteration_no,b.n_iterations,  b.t_cluster as t_cluster_whole, d.t_cluster as t_cluster_cumulative,  d.f_score as adj_random_index, d.ami
+    from webApp_datapoint as d join webApp_benchmark as b on d.benchmark_id == b.id
+    where d.benchmark_id in (
+        select max(b.id)
+        from webApp_datapoint as d join webApp_benchmark as b on d.benchmark_id == b.id
+        where d.id not NULL
+        group by b.algo_type)'''
+
+    with connection.cursor() as cursor:
+        data = cursor.execute(query_string, []).fetchall()
+
+    series_by_dataset = defaultdict(list)
+    for iteration in data:
+        key, value = iteration[0:3], iteration[3:]
+        series_by_dataset[key].append(value)
+
+    time_series = []
+    ari_series = []
+    for key in series_by_dataset.keys():
+        t = {
+            'name': key,
+            'data': []
+        }
+        ar = {
+            'name': key,
+            'data': []
+        }
+        for value in series_by_dataset[key]:
+            print(value)
+            t['data'].append((value[0], value[3]))  # it_no,t_cum
+            ar['data'].append([value[0], value[4], value[5]])  # it_no,ari
+        time_series.append(t)
+        ari_series.append(ar)
+
+    series = {
+        "time": time_series,
+        "ari": ari_series
+    }
+
+    return series
