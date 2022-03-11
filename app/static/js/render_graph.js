3e773f7dc0830abd72d44ac806e4b5164126688f
MaloJaffre <jaffre.malo@gmail.com>
2022-03-11 07:30:06 +0100

    Improve rendering of graph

--- app/static/js/render_graph.js
+++ app/static/js/render_graph.js
@@ -8,8 +8,12 @@ var svg = d3.select('#graph'),
 d3.csv('/node.csv', function (d) {
   d.id = +d.id;
   d.name = d.id;
-  d.color = +d.id;
+  d.color = +d.new;
   d.number = +d.number;
+  d.size = "Size: " + d.number + "\n";
+  if(d.old_number != "Nan") {
+    d.size += "Old size: " + d.old_number + "\n";
+  }
   return d;
 }).then(function (dnodes) {
   var dlinks = []
@@ -26,11 +30,11 @@ d3.csv('/node.csv', function (d) {
       .selectAll('.edge')
       .data(dlinks)
       .enter()
-      .append('line')
+      .append('polyline')
       .attr('class', 'edge')
-      .attr('marker-end', 'url(#arrowhead)')
+      .attr('marker-mid', 'url(#arrowhead)')
       .style('stroke', function (d) {
-        return 'green';
+        return 'grey';
       });
 
     var link_text = g
@@ -38,7 +42,7 @@ d3.csv('/node.csv', function (d) {
       .data(dlinks)
       .enter()
       .append('text')
-      .attr('style', 'font-size : x-small')
+      // .attr('style', 'font-size : x-small')
       .text(function (d) {
         return d.tag;
       });
@@ -53,13 +57,13 @@ d3.csv('/node.csv', function (d) {
 
     node
       .append('circle')
-      .attr('r', function(d){ return 4*Math.log(d.number)} )
-      .style('fill', function (d, i) {
+      .attr('r', function(d) { return 4*Math.log(d.number + 1) } )
+      .style('fill', function (d) {
         return colors(d.color);
       });
 
     node.append('title').text(function (d) {
-      return d.properties;
+      return d.size + "Properties:\n" + d.properties.replaceAll(":", "\n");
     });
 
     node
@@ -70,19 +74,19 @@ d3.csv('/node.csv', function (d) {
         return d.labels;
       });
 
-    node
-      .append('text')
-      .attr('dx', 15)
-      .attr('dy', 40)
-      .attr('style', 'font-size : x-small')
-      .text(function (d) {
-        if (d.properties.length > 25) {
-          return d.properties.slice(0, 25) + '...';
-        }
-        return d.properties;
-      });
-    console.log(dnodes);
-    console.log(dlinks);
+    // node
+    //   .append('text')
+    //   .attr('dx', 15)
+    //   .attr('dy', 40)
+    //   .attr('style', 'font-size : x-small')
+    //   .text(function (d) {
+    //     if (d.properties.length > 25) {
+    //       return d.properties.slice(0, 25) + '...';
+    //     }
+    //     return d.properties;
+    //   });
+    // console.log(dnodes);
+    // console.log(dlinks);
     d3
       .forceSimulation(dnodes)
       .force(
@@ -114,24 +118,19 @@ d3.csv('/node.csv', function (d) {
   }
     function update() {
       link
-        .attr('x1', function (d) {
-          return d.source.x;
-        })
-        .attr('y1', function (d) {
-          return d.source.y;
-        })
-        .attr('x2', function (d) {
-          return d.target.x;
-        })
-        .attr('y2', function (d) {
-          return d.target.y;
-        });
+      .attr("points", function(d) {
+        return [
+             d.source.x, d.source.y,
+             d.source.x/4 + 3 * d.target.x/4, d.source.y/4 + 3*d.target.y/4,
+             d.target.x, d.target.y
+        ].join(',');
+      });
       link_text.attr('transform', function (d) {
         return (
           'translate(' +
-          (d.source.x + d.target.x) / 2 +
+          (d.source.x + 3 * d.target.x) / 4 +
           ', ' +
-          (d.source.y + d.target.y) / 2 +
+          (d.source.y + 3 * d.target.y) / 4 +
           ')'
         );
       });
